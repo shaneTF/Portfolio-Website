a9a7da9cde3bb2d75b034232e4cd8b501960c83e
Shane Franklin <shanefranklin148@gmail.com>
2023-03-12 20:06:32 -0400

    Header and header css added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assests/ShaneFranklin.jpg'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello! I am</h5>
+        <h1>Shane Franklin</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt='me' />
+      </div>
+
+      <a href="#contact" className='scroll__down'>Scroll Down</a>
+    </div>
+      
+    </header>
   )
 }
 
