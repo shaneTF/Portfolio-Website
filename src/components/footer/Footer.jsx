14c05878b0c09aad27bacad94b8509e6df43c9b8
Shane Franklin <shanefranklin148@gmail.com>
2023-03-13 15:56:04 -0400

    Footer sections added and minor nav color change

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './footer.css'
+import {BsGithub} from 'react-icons/bs'
+import {FiTwitter} from 'react-icons/fi'
+import {FiInstagram} from 'react-icons/fi'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href='#' className='footer_logo'>SHANE FRANKLIN</a>
+
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#services'>Services</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#testimonials'>Testimonials</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href="https://github.com"><BsGithub/></a>
+        <a href="https://twitter.com"><FiTwitter/></a>
+        <a href="https://instagram.com"><FiInstagram/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Shane Franklin. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
