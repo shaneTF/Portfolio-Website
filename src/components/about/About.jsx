8054f19158f478fa3a42c87d16f3ceb63c00e8b5
Shane Franklin <shanefranklin148@gmail.com>
2023-03-13 06:19:45 -0400

    Nav and about additions, moved index for github pages

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assests/52.jpg'
+import {BsPersonWorkspace} from 'react-icons/bs'
+import {AiOutlineFundProjectionScreen} from 'react-icons/ai'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <BsPersonWorkspace className='about__icon'/>
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+
+            <article className='about__card'>
+              <AiOutlineFundProjectionScreen className='about__icon'/>
+              <h5>Projects</h5>
+              <small>2 Official Projects</small>
+              <small>1 Unofficial Project</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis ut beatae nisi natus, doloremque blanditiis corporis nemo autem velit nesciunt doloribus recusandae, porro omnis neque laborum fuga voluptas, quae commodi.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk!</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
