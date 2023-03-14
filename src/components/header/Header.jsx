a562a1fa2595d50b94bebd707ffab0db53edcc8c
Shane Franklin <shanefranklin148@gmail.com>
2023-03-14 10:51:10 -0400

    Changed some info and updated homepage

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello! I am</h5>
         <h1>Shane Franklin</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
+        <h5 className="text-light">Software Engineer</h5>
         <CTA />
         <HeaderSocials />
 
